3532a113ff3677dfaf54f0daf16f84285217eb01
PureteGO <puretegoonline@gmail.com>
2026-02-02 22:31:53 -0300

    Trigger Deploy: Restart App

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,4 +1,6 @@
 from app import create_app
 from config.settings import ProductionConfig
 
+# Application Entry Point for cPanel via Passenger
+# Last Updated: 2026-02-02
 application = create_app(ProductionConfig)
